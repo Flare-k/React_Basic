f222612740eb74f5e1a183fe15b6f0c00342e2b1
rokkyw <rokkyw@naver.com>
2021-05-16 16:25:56 +0900

    검색 기능 구현 | sort, filter

--- react-basic/src/components/App.js
+++ react-basic/src/components/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: ''
-        };
-        this.addMore = this.addMore.bind(this);
-    }
-
-    addMore() {
-        this.setState({
-            name : "Kang"
-        });
-    }
-
     render() {
         return(
-            <div>
-                <button onClick={this.addMore}>Click Me</button>
-                <h1>Hello!!! {this.state.name}</h1>
-            </div>
+            <Contact/>
         );
     }
 }
--- react-basic/src/components/Contact.js
+++ react-basic/src/components/Contact.js
@@ -2,30 +2,51 @@ import React from 'react';
 import ContactInfo from './ContactInfo';
 
 export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contactData : [
-        {name : "Kang", phone:"010-0001-0001"},
-        {name : "Yeon", phone:"010-0020-0002"},
-        {name : "Wook", phone:"010-0300-0003"},
-        {name : "Choi", phone:"010-4000-0004"}
-      ]
-    };
-  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: '',
+            contactData : [
+            {name : "Kang", phone:"010-0001-0001"},
+            {name : "Yeon", phone:"010-0020-0002"},
+            {name : "Wook", phone:"010-0300-0003"},
+            {name : "Choi", phone:"010-4000-0004"}]
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+    }
   
-  render() {
+    // input에 입력이 가능하기 위해서는 target이 변하는걸 감지해야 한다.
+    handleChange(e) {
+        this.setState({
+            keyword: e.target.value
+        });
+    }
+
+    render() {
     
-    const mapToComponent = (data) => {
-      return data.map((contact, i) => {
-        return (<ContactInfo contact={contact} key={i}/>);
-      });
-    };
+        const mapToComponent = (data) => {
+            data.sort();
+            data = data.filter((contact) => {
+                return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;   // name에 search에 입력된 내용이 포함되어 있다면 true
+            })
+
+            return data.map((contact, i) => {
+                return (<ContactInfo contact={contact} key={i}/>);
+            });
+        };
     
-    return (
-      <div>
-        {mapToComponent(this.state.contactData)}
-      </div>
-    );
-  }
+        return (
+            <div>
+                <h1>Contacts</h1>
+                <input 
+                    name="keyword"
+                    placeholder='Search'
+                    value={this.state.keyword}
+                    onChange={this.handleChange}
+                />
+                <div>{mapToComponent(this.state.contactData)}</div>
+            </div>
+        );
+    }
 };
